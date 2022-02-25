34a15937affadf40cb8343dbc6ead1555a9b1534
Harshavardhan Musanalli <Harshavardhan.Musanalli@amadeus.com>
2022-02-25 15:33:40 +0100

    Changes

--- scripts/update-versions-file.sh
+++ scripts/update-versions-file.sh
@@ -2,36 +2,6 @@
 set -e
 
 provider=$(echo $PROJECT_NAME | rev | cut -d- -f1 | rev)
-## Check whether new_version file exists in the current dir
-## This crappy logic is to avoid concurrent runs to push same versions file onto artifactory
-## if we use commercial version of goreleaser, this can be avoided with post hooks
-## neverthless, this is for improvement. It first checks whether new_version file exists, if yes it skips all execution..
-## so that only one build runs parallelly
-if [[ $LINUX_AMD64 -eq "linux_amd64" ]]; then
-  ## extract the provider name from the project name
-
-  ## download the versions file if exists
-  response=$(curl -k -Is ${ARTIFACTORY_URL}/artifactory/generic-production-iac/terraform/providers/v1/amadeus/$provider/versions | head -1 | cut -d ' ' -f2)
-
-  if [[ $response -eq "200" ]]; then
-    echo "versions file exists, downloading it"
-    curl -k -s -o versions ${ARTIFACTORY_URL}/artifactory/generic-production-iac/terraform/providers/v1/amadeus/$provider/versions
-    create_version
-    update_version
-    upload_to_artifactory
-  elif [[ $response -eq "404" ]]; then
-    echo "versions file does not exist, it's a new provider"
-    create_version
-    create_initial_version
-    upload_to_artifactory
-  else
-    echo "Some error while downloading versions file from artifactory"
-    exit 1
-  fi
-else
-  echo "not linux_amd64, skipping rest of the execution"
-fi
-
 ## create version from the passed one
 function create_version() {
   cat <<EOF >new_version
@@ -76,5 +46,35 @@ function create_initial_version() {
 function upload_to_artifactory() {
   curl -k --user $ARTIFACTORY_USER:$ARTIFACTORY_PASSWORD --upload-file all_versions -X PUT "${ARTIFACTORY_URL}/artifactory/generic-production-iac/terraform/providers/v1/amadeus/$provider/versions"
 }
+
+## Check whether new_version file exists in the current dir
+## This crappy logic is to avoid concurrent runs to push same versions file onto artifactory
+## if we use commercial version of goreleaser, this can be avoided with post hooks
+## neverthless, this is for improvement. It first checks whether new_version file exists, if yes it skips all execution..
+## so that only one build runs parallelly
+if [[ $LINUX_AMD64 -eq "linux_amd64" ]]; then
+  ## extract the provider name from the project name
+
+  ## download the versions file if exists
+  response=$(curl -k -Is ${ARTIFACTORY_URL}/artifactory/generic-production-iac/terraform/providers/v1/amadeus/$provider/versions | head -1 | cut -d ' ' -f2)
+
+  if [[ $response -eq "200" ]]; then
+    echo "versions file exists, downloading it"
+    curl -k -s -o versions ${ARTIFACTORY_URL}/artifactory/generic-production-iac/terraform/providers/v1/amadeus/$provider/versions
+    create_version
+    update_version
+    upload_to_artifactory
+  elif [[ $response -eq "404" ]]; then
+    echo "versions file does not exist, it's a new provider"
+    create_version
+    create_initial_version
+    upload_to_artifactory
+  else
+    echo "Some error while downloading versions file from artifactory"
+    exit 1
+  fi
+else
+  echo "not linux_amd64, skipping rest of the execution"
+fi
 
 echo $?
