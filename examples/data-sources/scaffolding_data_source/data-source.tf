5507a7fe04707db6f13ed1232560a6f682c637a8
Harshavardhan Musanalli <harshavmb@gmail.com>
2022-07-27 14:57:05 +0200

    ip provider release version 1.0.0

--- examples/data-sources/scaffolding_data_source/data-source.tf
+++ examples/data-sources/scaffolding_data_source/data-source.tf
@@ -1,3 +1,3 @@
-data "scaffolding_data_source" "example" {
-  sample_attribute = "foo"
+data "ip" "example" {
+  nw_interface = "eth0"
 }
--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,3 @@
-provider "scaffolding" {
+provider "ip" {
   # example configuration here
 }
